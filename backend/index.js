2b2c050f938ed4aa324ce086b19491a4628bc41f
onowakow <nowakowskiowen08@gmail.com>
2021-11-09 13:22:29 -0600

    Added delete and post methods

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
-const { response } = require('express');
-const express = require('express')
+
+// Different syntax, but similar idea to 'import express from 'express'
+const express = require('express');
+// express() is a function, creating an express application stored in 'app'
 const app = express()
+app.use(express.json())
 
 let notes = [
   {
@@ -29,6 +32,53 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  // request object (related to above URL), can be accessed with request
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log(id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(note => note.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    id: generateId,
+    date: new Date(),
+    important: body.important || false,
+    content: body.content
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 const PORT = 3001;
